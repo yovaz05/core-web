2ad3a03d47fc3241aaccdef78a27a2b28acc79e2
danielomarromero@gmail.com <danielomarromero@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2013-05-16 16:34:16 +0000

    id negativo en los MyArray y MyPair

---
+++ trunk/WEB-INF/src/com/coreweb/util/MyAuxObject.java
@@ -0,0 +1,48 @@
+package com.coreweb.util;
+
+import com.coreweb.domain.IiD;
+
+public abstract class MyAuxObject implements IiD {
+	
+	
+	private static long ID_NUEVO  = -1;
+
+	Long id = new Long(getNewId());
+
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+
+
+	@Override
+	public void setId(Long id) {
+		// TODO Auto-generated method stub
+		this.id = id;
+	}
+
+	public static synchronized long getNewId(){
+		ID_NUEVO--;
+		return ID_NUEVO;
+	}
+	
+	@Override
+	public boolean esNuevo() {
+		// TODO Auto-generated method stub
+		return this.id.longValue() < 0;
+	}
+
+	
+	public boolean equals(Object o) {
+		boolean b = false;
+		try {
+			MyAuxObject aux = (MyAuxObject)o;
+			b = ((this.getId().longValue() == aux.getId().longValue())); //  &&(this.getText().compareTo(aux.getText())==0));
+		} catch (Exception e) {
+			b = false;
+		}
+		return b;
+	}
+
+}
--- trunk/WEB-INF/src/com/coreweb/util/MyPair.java
+++ trunk/WEB-INF/src/com/coreweb/util/MyPair.java
@@ -6,7 +6,7 @@ import com.coreweb.domain.IiD;
 
 
 
-public class MyPair  implements IiD {
+public class MyPair extends MyAuxObject {
 
 	//private Long id = new Long(-1);
 	private String text = " ";
@@ -15,32 +15,6 @@ public class MyPair  implements IiD {
 	}
 
 	
-	
-	private static long ID_NUEVO  = -1;
-	Long id = new Long(ID_NUEVO);
-
-	@Override
-	public Long getId() {
-		// TODO Auto-generated method stub
-		return this.id;
-	}
-
-
-	@Override
-	public void setId(Long id) {
-		// TODO Auto-generated method stub
-		this.id = id;
-	}
-
-
-	@Override
-	public boolean esNuevo() {
-		// TODO Auto-generated method stub
-		return this.id.longValue() == ID_NUEVO;
-	}
-
-
-
 	public MyPair(long id, String text){
 		this.setId(id);
 		this.text = text;
@@ -59,14 +33,5 @@ public class MyPair  implements IiD {
 		return this.text + " ("+this.getId()+")";
 	}
 	
-	public boolean equals(Object o) {
-		boolean b = false;
-		try {
-			MyPair aux = (MyPair)o;
-			b = ((this.getId().longValue() == aux.getId().longValue())); //  &&(this.getText().compareTo(aux.getText())==0));
-		} catch (Exception e) {
-			b = false;
-		}
-		return b;
-	}
+
 }
