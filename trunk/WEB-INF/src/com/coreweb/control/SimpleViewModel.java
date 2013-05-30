b0b19cc713a93342815a191975813f1e97b21343
danielomarromero@gmail.com <danielomarromero@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2013-05-30 16:07:16 +0000

    Corrección de manejo de alias de formulario cuando se usa el SimpleViewModel.

--- trunk/WEB-INF/src/com/coreweb/control/SimpleViewModel.java
+++ trunk/WEB-INF/src/com/coreweb/control/SimpleViewModel.java
@@ -1,7 +1,10 @@
 package com.coreweb.control;
 
 import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.zk.ui.Execution;
 
 /**
  * Para ser usada en ventanas que no pertenecen a un template, por ejemplo, VindowsPopup
@@ -12,13 +15,18 @@ public abstract class SimpleViewModel extends GenericViewModel {
 
 	
 	@Init(superclass = true)
-	public void initSimpleViewModel(){
-		
+	public void initSimpleViewModel(@ContextParam(ContextType.EXECUTION) Execution execution){
+		String aliasForm = (String) execution.getAttribute("aliasForm");
+		String labelF = this.getUs().formLabel(aliasForm);
+		this.setTextoFormularioCorriente(labelF);
+		this.setAliasFormularioCorriente(aliasForm);	
 	}
 	
 	@AfterCompose(superclass = true)
 	public void afterComposeSimpleViewModel(){
 		this.addCamposObligotorios(this.mainComponent);
+		
+		
 	}
 	
 	
