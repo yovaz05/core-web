261e446c69cecf5d1f07350954390262a7daac2b
danielomarromero@gmail.com <danielomarromero@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2013-07-17 05:28:57 +0000

    metodo Existe en el Register

--- WEB-INF/src/com/coreweb/IDCore.java
+++ WEB-INF/src/com/coreweb/IDCore.java
@@ -8,4 +8,12 @@ public class IDCore {
 	public static String O_EDITAR = "Editar";
 	public static String O_ELIMINAR = "Eliminar";
 	public static String O_BUSCAR = "Buscar";
+	
+	
+	
+	public static String TIPO_STRING = "String";
+	public static String TIPO_NUMERICO = "Numerico";
+	public static String TIPO_BOOL = "Bool";
+	public static String TIPO_DATE = "Date";
+
 }
