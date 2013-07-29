f807e555835fca48516814e5183ed5f35eadc7fa
sergioraul777@gmail.com <sergioraul777@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2013-07-29 19:29:55 +0000

    Agregado control de límites y números provisorios en AutoNumero

--- WEB-INF/src/com/coreweb/util/AutoNumeroControl.java
+++ WEB-INF/src/com/coreweb/util/AutoNumeroControl.java
@@ -12,35 +12,80 @@ public class AutoNumeroControl {
 	 */
 	
 	static Misc m = new Misc();
+	static long SIN_LIMITE = -1;
 	
+	public synchronized static void setAutoNumero(String key, long ini, String descripcion) throws Exception {
+		setAutoNumero(key,  ini, SIN_LIMITE, descripcion);
+	}
+	
+	public synchronized static void setAutoNumero(String key, long ini, long numeroHasta, String descripcion) throws Exception {
+		Register r = Register.getInstance();
+		AutoNumero an = r.getAutoNumero(key);
+		if (an == null) {
+			an = new AutoNumero();
+			an.setKey(key);
+			an.setNumero(ini-1);
+			an.setNumeroHasta(numeroHasta);
+			an.setDescripcion(descripcion);
+		}else{
+			throw new Exception("Ya existe el "+key);
+		}
+
+	}
+
 	public synchronized static long getAutoNumero(String key) throws Exception {
+		return getAutoNumero(key, false);
+	}
+
+	
+	public synchronized static long getAutoNumero(String key, boolean provisorio) throws Exception {
 		long out = 0;
+		long hasta = 0;
 
 		Register r = Register.getInstance();
 		AutoNumero an = r.getAutoNumero(key);
 		if (an == null) {
 			an = new AutoNumero();
 			an.setKey(key);
-			an.setId(out);
+			an.setNumero(out);
 			an.setDescripcion("auto generado");
 		}
 
 		out = an.getNumero() + 1;
-		an.setNumero(out);
-		an.setFecha(new Date());
-		r.saveObject(an);
+		hasta = an.getNumeroHasta();
+		if ((hasta != SIN_LIMITE)&&(out > hasta)){
+			throw new Exception("Límite de numeración excedida ["+hasta+"]");
+		}
+		
+		
+		
+		if (provisorio == false){
+			an.setNumero(out);
+			an.setFecha(new Date());
+			r.saveObject(an);
+		}
 
 		return out;
 	}
 
 	public synchronized static String getAutoNumero(String key, int ceros) throws Exception {
-		long i = getAutoNumero(key);
+		return getAutoNumero(key, ceros, false);
+	}
+
+	
+	public synchronized static String getAutoNumero(String key, int ceros, boolean provisorio) throws Exception {
+		long i = getAutoNumero(key, provisorio);
 		String out = m.ceros(i+"", ceros);
 		return out;
 	}
 
+	
 	public synchronized static String getAutoNumeroKey(String key, int ceros) throws Exception {
-		String out = getAutoNumero(key, ceros);
+		return getAutoNumeroKey(key, ceros, false);
+	}
+	
+	public synchronized static String getAutoNumeroKey(String key, int ceros, boolean provisorio) throws Exception {
+		String out = getAutoNumero(key, ceros, provisorio);
 		return (key+"-"+out);
 	}
 	
