bfb2aa9db96834fd60cdd483f9f7be5a2c3a7b0e
danielomarromero@gmail.com <danielomarromero@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2013-08-24 21:27:02 +0000

    archivos de ejecucion, Daniel

--- trunk/WEB-INF/copyDaniel.sh
+++ trunk/WEB-INF/copyDaniel.sh
@@ -3,7 +3,6 @@
 CORE=/home/daniel/datos/yhaguy/proyecto-core/core
 YHAGUY=/home/daniel/datos/yhaguy/proyecto-core/yhaguy
 VIDRIOLUZ=/home/daniel/datos/vidrio-luz/proyecto/vidrioluz
-TDNAPP=/home/daniel/datos/tdn/tdnapp
 
 ########### yhaguy ##########################
 DESDE=$CORE
@@ -30,7 +29,7 @@ zip -r core com/*  -x *.svn*
 mv core.zip core.jar
 mv core.jar  $HASTA/WEB-INF/lib/
 
-########### vidrio luz ##########################
+########### vidrioluz ##########################
 DESDE=$CORE
 HASTA=$VIDRIOLUZ
 
@@ -54,31 +53,4 @@ cd ../src
 zip -r core com/*  -x *.svn*
 mv core.zip core.jar
 mv core.jar  $HASTA/WEB-INF/lib/
-
-
-########### tdnapp ##########################
-DESDE=$CORE
-HASTA=$TDNAPP
-
-cp $DESDE/WEB-INF/lang-addon.xml $HASTA/WEB-INF/lang-addon.xml
-
-# Archivos .zul
-cd $DESDE
-zip -r core core/*
-mv core.zip  $HASTA/
-cd $HASTA
-unzip -o core.zip
-#chmod 777 -R core
-rm core.zip
-
-# Archivos .class (crea jar)
-
-cd $CORE/WEB-INF/classes
-zip -r core com/*  -x *.svn*
-mv core.zip ../src
-cd ../src
-zip -r core com/*  -x *.svn*
-mv core.zip core.jar
-mv core.jar  $HASTA/WEB-INF/lib/
-
 
