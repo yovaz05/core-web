26a383771fc62fb5df2c27f3cf991aa8a9456f17
danielomarromero@gmail.com <danielomarromero@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2013-08-25 08:15:28 +0000

    ajustes para el zk7

--- trunk/WEB-INF/copyDaniel.sh
+++ trunk/WEB-INF/copyDaniel.sh
@@ -3,6 +3,7 @@
 CORE=/home/daniel/datos/yhaguy/proyecto-core/core
 YHAGUY=/home/daniel/datos/yhaguy/proyecto-core/yhaguy
 VIDRIOLUZ=/home/daniel/datos/vidrio-luz/proyecto/vidrioluz
+TDNPEDIDOS=/home/daniel/datos/tdn/pedidos/pedidosApp
 
 ########### yhaguy ##########################
 DESDE=$CORE
@@ -53,4 +54,31 @@ cd ../src
 zip -r core com/*  -x *.svn*
 mv core.zip core.jar
 mv core.jar  $HASTA/WEB-INF/lib/
+
+
+########### TDN Pedidos ##########################
+DESDE=$CORE
+HASTA=$TDNPEDIDOS
+
+cp $DESDE/WEB-INF/lang-addon.xml $HASTA/WEB-INF/lang-addon.xml
+
+# Archivos .zul
+cd $DESDE
+zip -r core core/*
+mv core.zip  $HASTA/
+cd $HASTA
+unzip -o core.zip
+#chmod 777 -R core
+rm core.zip
+
+# Archivos .class (crea jar)
+
+cd $CORE/WEB-INF/classes
+zip -r core com/*  -x *.svn*
+mv core.zip ../src
+cd ../src
+zip -r core com/*  -x *.svn*
+mv core.zip core.jar
+mv core.jar  $HASTA/WEB-INF/lib/
+
 
--- trunk/WEB-INF/src/com/coreweb/componente/BuscarElemento.java
+++ trunk/WEB-INF/src/com/coreweb/componente/BuscarElemento.java
@@ -1,6 +1,5 @@
 package com.coreweb.componente;
 
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
