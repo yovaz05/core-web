a29e857925088efd7c1001cfcef0773fcd208957
vereojeda@gmail.com <vereojeda@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2013-10-01 15:30:31 +0000

    Cambio de parametros en el el comparador de array de objetos.

--- WEB-INF/src/com/coreweb/util/ComparadorArrayObjetos.java
+++ WEB-INF/src/com/coreweb/util/ComparadorArrayObjetos.java
@@ -2,16 +2,23 @@ package com.coreweb.util;
 
 import java.util.Comparator;
 
-public class ComparadorArrayObjetos implements Comparator<Object[]>{
+public class ComparadorArrayObjetos implements Comparator<Object[]> {
 
-	int n;
-	
-	public ComparadorArrayObjetos(int n){
-		this.n = n;
+	int[] n;
+
+	public ComparadorArrayObjetos(int... param) {
+		this.n = param;
 	}
+
 	@Override
 	public int compare(Object[] o1, Object[] o2) {
-		return (o2[this.n].toString()).compareTo(o1[this.n].toString());
+		String a = "";
+		String b = "";
+		for (int i = 0; i < this.n.length; i++) {
+			a = a + o1[this.n[i]].toString();
+			b = b + o2[this.n[i]].toString();
+		}
+		return (b.compareTo(a));
 	}
-	
+
 }
