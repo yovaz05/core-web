2d02490301e1805e46c9e99ebc206b590d44a8fc
vereojeda@gmail.com <vereojeda@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2013-11-22 15:32:31 +0000

    metodos crearAlertas (todavia no estan terminados)

--- WEB-INF/src/com/coreweb/dto/UtilCoreDTO.java
+++ WEB-INF/src/com/coreweb/dto/UtilCoreDTO.java
@@ -22,14 +22,61 @@ public class UtilCoreDTO extends DTO {
 	public void setHabilitado(List<MyPair> habilitado) {
 		this.habilitado = habilitado;
 	}
-	
-	
+
 	// ==== Para el manejo de propiedades del usuario
-	
-	public String getPropiedad(String key){
+
+	public String getPropiedad(String key) {
 		String out = "";
-		
+
 		return out;
+	}
+
+	// ==== Para alertas
+
+	MyPair tipoAlertaUno = new MyPair();
+	MyPair tipoAlertaComunitaria = new MyPair();
+	MyPair tipoAlertaMuchos = new MyPair();
+	MyPair nivelAlertaInformativa = new MyPair();
+	MyPair nivelAlertaError = new MyPair();
+
+	public MyPair getTipoAlertaUno() {
+		return tipoAlertaUno;
+	}
+
+	public void setTipoAlertaUno(MyPair tipoAlertaUno) {
+		this.tipoAlertaUno = tipoAlertaUno;
+	}
+
+	public MyPair getTipoAlertaComunitaria() {
+		return tipoAlertaComunitaria;
+	}
+
+	public void setTipoAlertaComunitaria(MyPair tipoAlertaComunitaria) {
+		this.tipoAlertaComunitaria = tipoAlertaComunitaria;
+	}
+
+	public MyPair getTipoAlertaMuchos() {
+		return tipoAlertaMuchos;
+	}
+
+	public void setTipoAlertaMuchos(MyPair tipoAlertaMuchos) {
+		this.tipoAlertaMuchos = tipoAlertaMuchos;
+	}
+
+	public MyPair getNivelAlertaInformativa() {
+		return nivelAlertaInformativa;
+	}
+
+	public void setNivelAlertaInformativa(MyPair nivelAlertaInformativa) {
+		this.nivelAlertaInformativa = nivelAlertaInformativa;
+	}
+
+	public MyPair getNivelAlertaError() {
+		return nivelAlertaError;
+	}
+
+	public void setNivelAlertaError(MyPair nivelAlertaError) {
+		this.nivelAlertaError = nivelAlertaError;
 	}
 
 }
