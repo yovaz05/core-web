80ce121d8573ac5f2d12fccae16b70980c9795f0
danielomarromero@gmail.com <danielomarromero@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2014-05-21 23:47:53 +0000

    algunos cambios en el toolbar, pero aún no está terminado

--- trunk/WEB-INF/src/com/coreweb/IDCore.java
+++ trunk/WEB-INF/src/com/coreweb/IDCore.java
@@ -7,6 +7,7 @@ public class IDCore {
 	public static String O_AGREGAR = "Agregar";
 	public static String O_EDITAR = "Editar";
 	public static String O_ELIMINAR = "Eliminar";
+	public static String O_ANULAR = "Anular";
 	public static String O_BUSCAR = "Buscar";
 	public static String F_MIS_ALERTAS = "MisAlertas";
 }
