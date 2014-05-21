26412c1d975a996bf2a023987da5a6858055b157
sergioraul777@gmail.com <sergioraul777@gmail.com@d1b1326f-d3a5-52b6-1ddb-2d6046a30c21>
2014-05-21 19:15:54 +0000

    guaranibox y dolarbox con mascara y simbolo.. Agregado fechabox...

---
+++ WEB-INF/src/com/coreweb/extras/macros/CustomDoublebox.java
@@ -0,0 +1,66 @@
+package com.coreweb.extras.macros;
+
+import org.zkoss.zul.Doublebox;
+
+public class CustomDoublebox extends Doublebox{
+
+	private static final long serialVersionUID = 1L;
+	
+	private static String TIPO_GUARANI = "guarani";
+	private static String TIPO_MONEDA_EXTRANJERA = "extranjera";	
+	
+	private static String FORMAT_LOCAL = "###,###,###,##0";
+	private static String FORMAT_EXTRANJERA = "###,###,##0.00";
+	
+	private String tipo = "";
+	private String simbolo = "";	
+	
+	public CustomDoublebox(){
+		super();
+	}	
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		
+		if (tipo.compareTo(TIPO_GUARANI) == 0) {
+			setWidgetListener("onBind", "jq(this).mask('000,000,000,000', {reverse: true});");	
+			
+		} else if (tipo.compareTo(TIPO_MONEDA_EXTRANJERA) == 0) {
+			setWidgetListener("onBind", "jq(this).mask('000,000,000.00', {reverse: true});");
+		} 
+		
+		this.tipo = tipo;
+	}
+
+	public String getSimbolo() {
+		return simbolo;
+	}
+
+	public void setSimbolo(String simbolo) {		
+		
+		if (tipo.compareTo(TIPO_GUARANI) == 0) {
+			setFormat(FORMAT_LOCAL + " " + simbolo);
+			
+		} else if (tipo.compareTo(TIPO_MONEDA_EXTRANJERA) == 0) {
+			setFormat(FORMAT_EXTRANJERA + " " + simbolo);
+			
+		} else {
+			setFormat("Error símbolo");
+		}
+		this.simbolo = simbolo;
+	}	
+
+	@Override
+	public Double getValue() {
+		return super.getValue();
+	}
+
+	@Override
+	public void setValue(Double value) {
+		this.setSimbolo(this.simbolo);
+		super.setValue(value);
+	}
+}
